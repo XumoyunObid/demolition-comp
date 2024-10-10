65305167bad31da8615f5280f68bb19e3fe765d7
Xumoyun <obidjonov06122005@gmail.com>
2024-10-11 04:37:56 +0500

    qewrty

--- src/Config/request.js
+++ src/Config/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const request = axios.create({ 
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "http://45.130.148.160:5555",
   headers: {
     'Content-Type': 'multipart/form-data',  // Set the content type to form-data
     'accept': 'text/plain'
